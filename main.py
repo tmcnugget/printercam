b3cd148ac4f7762219f3c7036fc7d7969d9640be
tmcnugget <graycday@icloud.com>
2025-02-01 15:20:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/feed')
 def video_feed():
     return Response(generate_frames(),
